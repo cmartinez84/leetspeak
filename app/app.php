3d812c56c1255cf9273637c641d947e39aa43bea
chris martienz and josh huffman <josh@>
2016-09-12 11:41:13 -0700

    generate pages with silex and twig

--- app/app.php
+++ app/app.php
@@ -2,11 +2,24 @@
 <?php
  require_once __DIR__."/../vendor/autoload.php";
  require_once __DIR__."/../src/Leetspeak.php";
+
  date_default_timezone_set('America/Los_Angeles');
+
  $app = new Silex\Application();
+
  $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
   ));
+
  $app->get("/", function() use ($app){
    return $app['twig']->render('home.html.twig');
  });
+
+ $app->post("/results", function() use ($app){
+   $newLeetspeak = new Leetspeak;
+   $results = $newLeetspeak->convertToLeetspeak($_POST['input_string']);
+   return $app['twig']->render('results.html.twig', array("results"=> $results));
+ });
+
+ return $app;
+?>
